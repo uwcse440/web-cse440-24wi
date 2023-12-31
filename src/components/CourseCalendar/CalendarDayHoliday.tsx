a0c72d579b2d520b18cfd1b9097c22eec8662b7d
James Fogarty <jayfo@users.noreply.github.com>
2023-12-30 19:01:40 -0800

    Initial Holiday Render

---
+++ src/components/CourseCalendar/CalendarDayHoliday.tsx
@@ -0,0 +1,24 @@
+import * as React from "react";
+
+import { CalendarItem, filterHolidayCalendarItems } from "@/types/CalendarData";
+import { Grid } from "@mui/material";
+
+export const CalendarDayHoliday: React.FunctionComponent<{
+  calendarItems: CalendarItem[];
+}> = ({ calendarItems }) => {
+  const holidayCalendarItems = filterHolidayCalendarItems(calendarItems);
+
+  if (holidayCalendarItems.length === 0) {
+    return null;
+  }
+
+  return holidayCalendarItems.map(
+    (itemCurrent, indexCurrent): React.ReactElement => {
+      return (
+        <Grid key={indexCurrent} item xs={12}>
+          {`Holiday: ${itemCurrent.title}`}
+        </Grid>
+      );
+    },
+  );
+};
