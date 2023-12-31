739e4ee2bfdf3fdc83fcc01a61fac967ca868f79
James Fogarty <jayfo@users.noreply.github.com>
2023-12-31 06:56:37 -0800

    Item Time and Location

---
+++ src/components/CourseCalendar/CalendarItemTimeAndLocations.tsx
@@ -0,0 +1,31 @@
+import * as React from "react";
+
+import { CalendarItem } from "@/types/CalendarData";
+import { Box } from "@mui/material";
+
+export const CalendarItemTimeAndLocations: React.FunctionComponent<{
+  calendarItem: CalendarItem;
+}> = ({ calendarItem }) => {
+  const calendarItemTimeAndLocations = (() => {
+    if ("timeAndLocation" in calendarItem) {
+      return [calendarItem.timeAndLocation];
+    } else if ("timeAndLocations" in calendarItem) {
+      return calendarItem.timeAndLocations;
+    } else {
+      return undefined;
+    }
+  })();
+
+  return (
+    calendarItemTimeAndLocations &&
+    calendarItemTimeAndLocations.map(
+      (timeAndLocationCurrent, indexCurrent): React.ReactElement => {
+        return (
+          <Box key={indexCurrent} sx={{ fontSize: "0.875rem" }}>
+            {`${timeAndLocationCurrent.time}, ${timeAndLocationCurrent.location}`}
+          </Box>
+        );
+      },
+    )
+  );
+};
