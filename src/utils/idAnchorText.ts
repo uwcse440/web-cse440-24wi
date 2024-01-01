21a963c3d48f18b880f6312a9348c696ec7bcebf
James Fogarty <jayfo@users.noreply.github.com>
2024-01-01 10:21:32 -0800

    Anchor Text

---
+++ src/utils/idAnchorText.ts
@@ -0,0 +1,10 @@
+/**
+ * Ensure text can be used in an id="text" anchor.
+ */
+export function idAnchorText(text: string): string {
+  return text
+    .toLowerCase()
+    .replace(/[^a-z0-9\- ]/g, "")
+    .replace(/[ ]/g, "-")
+    .replace(/[\-]+/g, "-");
+}
