f9a74f693cf019425a778e01c2ab7f5c9efacbc0
James Fogarty <jayfo@users.noreply.github.com>
2024-01-01 10:22:48 -0800

    Existing Types

---
+++ src/types/CourseStore.ts
@@ -0,0 +1,31 @@
+import { AssertionError } from "assert";
+
+export type CourseStoreLinkHREF = string;
+
+const CourseStoreLinkKeyValues = [
+  "linkCanvas",
+  "linkCanvasDiscussion",
+  "linkGitHub",
+  "linkUniversitySyllabusGuidelines",
+] as const;
+export type CourseStoreLinkKey = (typeof CourseStoreLinkKeyValues)[number];
+
+export function assertIsCourseStoreLinkKey(
+  courseStoreLinkKey: any,
+): asserts courseStoreLinkKey is CourseStoreLinkKey {
+  if (!CourseStoreLinkKeyValues.includes(courseStoreLinkKey)) {
+    throw new AssertionError({ message: "Invalid CourseStoreLinkKey" });
+  }
+}
+
+export interface CourseStoreData {
+  linkCanvas?: CourseStoreLinkHREF;
+  linkGitHub?: CourseStoreLinkHREF;
+  linkUniversitySyllabusGuidelines?: CourseStoreLinkHREF;
+}
+
+export interface CourseStore extends CourseStoreData {
+  linkCanvasDiscussion?: CourseStoreLinkHREF;
+}
+
+export default CourseStore;
