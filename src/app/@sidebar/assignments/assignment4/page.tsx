fc265232e215ce8603d4bf5a7d45da94ded54ddf
James Fogarty <jayfo@users.noreply.github.com>
2024-01-21 19:55:38 -0800

    Add Assignment Sidebar Links

--- src/app/@sidebar/assignments/assignment4/page.tsx
+++ src/app/@sidebar/assignments/assignment4/page.tsx
@@ -13,6 +13,28 @@ export default function Page(): React.ReactElement {
           anchor: "Assignment 4",
           href: "#assignment-4",
         },
+        [
+          {
+            anchor: "4web - Web Post",
+            href: "#assignment-4web-web-post",
+          },
+          {
+            anchor: "4web_final - Web Post",
+            href: "#assignment-4web-final-web-post",
+          },
+          {
+            anchor: "4poster - Poster and Pitch",
+            href: "#assignment-4poster-poster-and-pitch",
+          },
+          {
+            anchor: "4poster_final - Poster and Pitch",
+            href: "#assignment-4poster-final-poster-and-pitch",
+          },
+          {
+            anchor: "4poster_session - Poster Session",
+            href: "#assignment-4poster-session-poster-session",
+          },
+        ],
       ]}
     />
   );
--- src/utils/idAnchorText.ts
+++ src/utils/idAnchorText.ts
@@ -4,6 +4,7 @@
 export function idAnchorText(text: string): string {
   return text
     .toLowerCase()
+    .replace(/[_]/g, "-")
     .replace(/[^a-z0-9\- ]/g, "")
     .replace(/[ ]/g, "-")
     .replace(/[\-]+/g, "-");
