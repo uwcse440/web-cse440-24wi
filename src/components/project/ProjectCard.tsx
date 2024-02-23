54eca224f413a249bb66c22f6858b0e0f23d3125
James Fogarty <jayfo@users.noreply.github.com>
2024-02-22 20:37:13 -0800

    Code Format

--- src/components/project/ProjectCard.tsx
+++ src/components/project/ProjectCard.tsx
@@ -1,42 +1,36 @@
+import * as React from "react";
+import { FunctionComponent } from "react";
+
+import { AppLink } from "@/components/links/AppLink";
+import { Paths } from "@/paths.mjs";
+import { Card, CardContent, Stack } from "@mui/material";
 import Image from "next-image-export-optimizer";
 
-import * as React from 'react';
-import { FunctionComponent } from 'react';
-import { Paths } from "@/paths.mjs";
-
-import { AppLink } from '@/components/links/AppLink';
-
-import {
-    Card,
-    CardContent,
-    Stack,
-} from '@mui/material';
-
 interface ProjectCardProps {
-    name: string;
-    logo: string;
-    path: string;
+  name: string;
+  logo: string;
+  path: string;
 }
 
 export const ProjectCard: FunctionComponent<ProjectCardProps> = (props) => {
-    return (
-        <Card>
-            <CardContent>
-                <AppLink href={"/projects/" + props.path}>
-                    <Stack>
-                        { props.name }
-                        <Image
-                            src={props.logo}
-                            basePath={Paths.basePath}
-                            height="150"
-                            width="150"
-                            alt={ "Project Logo: " + props.name }
-                        />
-                    </Stack>
-                </AppLink>
-            </CardContent>
-        </Card>
-    );
-}
+  return (
+    <Card>
+      <CardContent>
+        <AppLink href={"/projects/" + props.path}>
+          <Stack>
+            {props.name}
+            <Image
+              src={props.logo}
+              basePath={Paths.basePath}
+              height="150"
+              width="150"
+              alt={"Project Logo: " + props.name}
+            />
+          </Stack>
+        </AppLink>
+      </CardContent>
+    </Card>
+  );
+};
 
 export default ProjectCard;
