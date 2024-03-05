3c4d0ae26916cbaf706239fc1e58acf8b57fea73
keliyk <keliyk@cs.washington.edu>
2024-03-05 12:48:50 -0800

    PupPals first draft

--- src/data/CalendarData.tsx
+++ src/data/CalendarData.tsx
@@ -326,14 +326,10 @@ export const calendarData: {
     },
     {
       type: "studio",
-      date: verifyCalendarDate("2024-03-05", "Tue"),
-      timeAndLocation: TIME_AND_LOCATION_LECTURE,
-      title: "Studio",
-      slides: "https://canvas.uw.edu/files/117252175/"
-    },
-    {
-      type: "studio",
-      date: verifyCalendarDate("2024-03-07", "Thu"),
+      dates: [
+        verifyCalendarDate("2024-03-05", "Tue"),
+        verifyCalendarDate("2024-03-07", "Thu"),
+      ],
       timeAndLocation: TIME_AND_LOCATION_LECTURE,
       title: "Studio",
     },
